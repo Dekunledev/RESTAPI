33bd6eb3e9e58cd6893da110eac9943684e88901
Dekunledev <kodujoko@gmail.com>
2022-05-09 01:32:41 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("CONNECTION OPEN");
+}
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
+
+
+app.listen(3000, () => {
+    console.log('LISTENING ON PORT!');
+});
